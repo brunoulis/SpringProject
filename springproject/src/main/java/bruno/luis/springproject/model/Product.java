5d709d5012ad4fa1d890f3afd06aee05abd9061e
brunoulis <luisvapa2012@gmail.com>
2023-08-15 17:39:17 +0200

    15/08/23

---
+++ springproject/src/main/java/bruno/luis/springproject/model/Product.java
@@ -0,0 +1,71 @@
+package bruno.luis.springproject.model;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private String description;
+    private String image;
+    private Double price;
+    private Integer quantity;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, String description, String image, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+}
