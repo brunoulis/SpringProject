6982b2a9fb2a123b43dc70008a2a05d2d2f034e7
brunoulis <luisvapa2012@gmail.com>
2023-08-17 17:01:12 +0200

    17/08/23

--- springprojectmaven/src/main/java/bruno/luis/springproject/MainApplication.java
+++ springprojectmaven/src/main/java/bruno/luis/springproject/MainApplication.java
@@ -2,10 +2,10 @@ package bruno.luis.springproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
+
+
+@SpringBootApplication 
 public class MainApplication {
 
 	public static void main(String[] args) {
