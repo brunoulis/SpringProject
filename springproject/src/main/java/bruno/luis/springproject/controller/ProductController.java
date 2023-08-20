5b15bd8cc2125504c5a7d183ae20744d924844ce
brunoulis <luisvapa2012@gmail.com>
2023-08-20 18:20:07 +0200

    20/08/23

--- springproject/src/main/java/bruno/luis/springproject/controller/ProductController.java
+++ springproject/src/main/java/bruno/luis/springproject/controller/ProductController.java
@@ -1,12 +1,19 @@
 package bruno.luis.springproject.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import bruno.luis.springproject.model.Product;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping("")
     public String show() {
@@ -16,6 +23,12 @@ public class ProductController {
     @GetMapping("/create")
     public String create() {
         return "products/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product) {
+        LOGGER.info("Este es el objeto product: {}", product);
+        return "redirect:/products";
     }
 
 }
