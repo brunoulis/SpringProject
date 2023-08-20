957d85037e2e9fd95c77d5fdc7daa40800f6420b
brunoulis <luisvapa2012@gmail.com>
2023-08-20 16:38:21 +0200

    17/08/23

--- springproject/src/main/java/bruno/luis/springproject/SpringprojectApplication.java
+++ springproject/src/main/java/bruno/luis/springproject/SpringprojectApplication.java
@@ -3,8 +3,7 @@ package bruno.luis.springproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication()
+@SpringBootApplication
 public class SpringprojectApplication {
 
 	public static void main(String[] args) {
