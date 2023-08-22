583033da9df85da8bc5628673252b4ba9dd86411
brunoulis <luisvapa2012@gmail.com>
2023-08-22 19:54:08 +0200

    UploadFileService

---
+++ springproject/src/main/java/bruno/luis/springproject/service/UploadFileService.java
@@ -0,0 +1,30 @@
+package bruno.luis.springproject.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileService {
+    private String folder = "images//";
+
+    public String saveImage(MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(folder + file.getOriginalFilename());
+                Files.write(path, bytes);
+                return path.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return folder;
+
+    }
+
+}
