32dc9505856accfe5459dad6e81333edb4cbc50f
brunoulis <luisvapa2012@gmail.com>
2023-08-30 19:45:32 +0200

    Mostrar imagenes en el munu principal

--- springproject/src/main/java/bruno/luis/springproject/controller/AdminController.java
+++ springproject/src/main/java/bruno/luis/springproject/controller/AdminController.java
@@ -1,15 +1,29 @@
 package bruno.luis.springproject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import bruno.luis.springproject.model.Product;
+import bruno.luis.springproject.service.ProductService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private ProductService produtoService;
+
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+
+        List<Product> products = produtoService.findAll();
+        model.addAttribute("products", products);
+        
         return "admin/home";
     }
 
