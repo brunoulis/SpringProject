caac31d3f09d75cc618ea72997694f59ee381bca
brunoulis <luisvapa2012@gmail.com>
2023-09-14 15:48:49 +0200

    security

--- springproject/src/main/java/bruno/luis/springproject/service/SpringBootSecurity.java
+++ springproject/src/main/java/bruno/luis/springproject/service/SpringBootSecurity.java
@@ -5,37 +5,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SpringBootSecurity implements WebSecurityConfigurer {
+public class SpringBootSecurity {
 
     @Autowired
     private UserDetailsService userDetailService;
 
-    @Override
-    public void init(SecurityBuilder builder) throws Exception {
-        ((AuthenticationManagerBuilder) builder).userDetailsService(userDetailService)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    public void configure(SecurityBuilder builder) throws Exception {
-        ((HttpSecurity) builder).authorizeRequests()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/products/**").hasRole("ADMIN")
-                .and().formLogin().loginPage("/user/login")
-                .permitAll().defaultSuccessUrl("/user/access");
-    }
-
     @Bean
     public BCryptPasswordEncoder getEnecoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/products/**").hasRole("ADMIN")
+                .and().formLogin().loginPage("/user/login")
+                .permitAll().defaultSuccessUrl("/user/access").and().build();
     }
 
 }
