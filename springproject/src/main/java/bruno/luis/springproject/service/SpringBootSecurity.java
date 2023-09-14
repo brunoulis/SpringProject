cc46e319beeab89e917c03f30a9ffebedee365a9
brunoulis <luisvapa2012@gmail.com>
2023-09-14 16:21:50 +0200

    mantenimiento

--- springproject/src/main/java/bruno/luis/springproject/service/SpringBootSecurity.java
+++ springproject/src/main/java/bruno/luis/springproject/service/SpringBootSecurity.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,16 +34,14 @@ public class SpringBootSecurity {
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeRequests()
-                /*
-                 * Este método define que las rutas `/admin/**` y `/products/**` solo pueden ser accedidas por usuarios con el rol `ADMIN`.
-                 */
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/products/**").hasRole("ADMIN")
-                /*
-                 * Este método define que las demás rutas pueden ser accedidas por cualquier usuario.
-                 */
-                .and().formLogin().loginPage("/user/login")
+        /*
+         * Este método define que las demás rutas pueden ser accedidas por cualquier
+         * usuario.
+         * Este método define que las rutas `/admin/**` y `/products/**` solo pueden ser
+         * accedidas por usuarios con el rol `ADMIN`.
+         */
+        return http.authorizeRequests().requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/products/**").hasRole("ADMIN").and().formLogin().loginPage("/user/login")
                 .permitAll().defaultSuccessUrl("/user/access").and().build();
     }
 
