6f62574c11887452b502c709cfd7b3606c90db24
brunoulis <luisvapa2012@gmail.com>
2023-09-14 16:07:34 +0200

    Comentarios Para mantenimiento de codigo

--- springproject/src/main/java/bruno/luis/springproject/service/SpringBootSecurity.java
+++ springproject/src/main/java/bruno/luis/springproject/service/SpringBootSecurity.java
@@ -1,10 +1,13 @@
 package bruno.luis.springproject.service;
 
+/*
+ * Esta clase es una clase de configuración de Spring Security.
+ */
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,16 +22,28 @@ public class SpringBootSecurity {
     @Autowired
     private UserDetailsService userDetailService;
 
+    /*
+     * Este método crea un bean de `BCryptPasswordEncoder`.
+     */
     @Bean
     public BCryptPasswordEncoder getEnecoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /*
+     * Este método crea un bean de `SecurityFilterChain`.
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeRequests()
+                /*
+                 * Este método define que las rutas `/admin/**` y `/products/**` solo pueden ser accedidas por usuarios con el rol `ADMIN`.
+                 */
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/products/**").hasRole("ADMIN")
+                /*
+                 * Este método define que las demás rutas pueden ser accedidas por cualquier usuario.
+                 */
                 .and().formLogin().loginPage("/user/login")
                 .permitAll().defaultSuccessUrl("/user/access").and().build();
     }
--- springproject/src/main/java/bruno/luis/springproject/service/UserDetailServiceImpl.java
+++ springproject/src/main/java/bruno/luis/springproject/service/UserDetailServiceImpl.java
@@ -1,5 +1,10 @@
 package bruno.luis.springproject.service;
 
+/*
+ * Esta clase implementa la interfaz `UserDetailsService`, que es utilizada por Spring Security para cargar
+ * los detalles del usuario desde la base de datos.
+ */
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,27 +21,55 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.servlet.http.HttpSession;
+
 @Service
+/*
+ * Esta anotación marca esta clase como un bean de Spring.
+ */
 public class UserDetailServiceImpl implements UserDetailsService {
 
+/*
+ * Este campo está inyectado con el bean `IUserService`, que proporciona métodos para acceder a la base de datos de usuarios.
+ */
     @Autowired
     private IUserService userService;
 
+/*
+ * Este campo se utiliza para encriptar contraseñas.
+ */
     private BCryptPasswordEncoder encoder;
 
+/*
+ * Este campo está inyectado con el bean `HttpSession`, que proporciona acceso a la sesión HTTP actual.
+ */
     @Autowired
     HttpSession session;
 
+/*
+ * Este campo es un registrador que se puede utilizar para escribir mensajes de registro.
+ */
     private Logger log = LoggerFactory.getLogger(UserDetailServiceImpl.class);
 
+/*
+ * Este método es utilizado por Spring Security para cargar los detalles del usuario desde la base de datos.
+ */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("Usuario que llega al login: {}", username);
+        /*
+         * Este método obtiene al usuario de la base de datos por su nombre de usuario.
+         */
         Optional<UserModel> optUser = userService.findByEmail(username);
         if (optUser.isPresent()) {
             log.info("Usuario que llega al login: {}", optUser.get().getId());
+            /*
+             * Este método establece al usuario en la sesión HTTP actual.
+             */
             session.setAttribute("user", optUser.get());
             UserModel user = optUser.get();
+            /*
+             * Este método crea un objeto `User` a partir del modelo de usuario.
+             */
             return User.builder().username(user.getName()).password(encoder.encode(user.getPassword()))
                     .roles(user.getType()).build();
 
